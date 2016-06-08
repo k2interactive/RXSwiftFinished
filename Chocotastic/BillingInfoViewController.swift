d3874c3a516e478e51ce6610c4850549433f5c66
Ellen Shapiro <designatednerd@gmail.com>
2016-06-07 22:31:26 -0700

    Get billing info and success VC up and running.

--- Chocotastic/BillingInfoViewController.swift
+++ Chocotastic/BillingInfoViewController.swift
@@ -12,28 +12,145 @@ import RxCocoa
 
 class BillingInfoViewController: UIViewController {
 
-    @IBOutlet private var creditCardNumberTextField: UITextField!
+    @IBOutlet private var creditCardNumberTextField: ValidatingTextField!
     @IBOutlet private var creditCardImageView: UIImageView!
-    @IBOutlet private var expirationDateTextField: UITextField!
-    @IBOutlet private var cvvTextField: UITextField!
-    @IBOutlet private var creditCardErrorLabel: UILabel!
+    @IBOutlet private var expirationDateTextField: ValidatingTextField!
+    @IBOutlet private var cvvTextField: ValidatingTextField!
+    @IBOutlet private var purchaseButton: UIButton!
     
-    let creditCardErrors: Variable<[String]> = Variable([])
+    let cardType: Variable<CardType> = Variable(.Unknown)
     let disposeBag = DisposeBag()
+    
+    //MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupCreditCardErrorDisplay()
+        setupCardImageDisplay()
+        setupTextChangeHandling()
     }
     
-    private func setupCreditCardErrorDisplay() {
-        creditCardErrors
+    //MARK: - RX Setup
+    
+    private func setupCardImageDisplay() {
+        cardType
             .asObservable()
-            .subscribeNext {
-                errors in
-                self.creditCardErrorLabel.text = errors.joinWithSeparator("\n")
-            }
+            .subscribeNext({
+                cardType in
+                self.creditCardImageView.image = cardType.image
+            })
             .addDisposableTo(disposeBag)
+    }
+    
+    private func setupTextChangeHandling() {
+        let creditCardValid = creditCardNumberTextField
+            .rx_text
+            .map { self.validateCardTextChange($0) }
+            .shareReplay(1)
+        
+        creditCardValid
+            .subscribeNext { self.creditCardNumberTextField.valid = $0 }
+            .addDisposableTo(disposeBag)
+        
+        let expirationValid = expirationDateTextField
+            .rx_text
+            .map { self.validateExpirationDateTextChange($0) }
+            .shareReplay(1)
+        
+        expirationValid
+            .subscribeNext { self.expirationDateTextField.valid = $0 }
+            .addDisposableTo(disposeBag)
+        
+        let cvvValid = cvvTextField
+            .rx_text
+            .map { self.validateCVVTextChange($0) }
+            .shareReplay(1)
+        
+        cvvValid
+            .subscribeNext { self.cvvTextField.valid = $0 }
+            .addDisposableTo(disposeBag)
+        
+        let everythingValid = Observable
+            .combineLatest(creditCardValid, expirationValid, cvvValid) {
+                $0 && $1 && $2 //All must be true
+            }
+            .shareReplay(1)
+        
+        everythingValid
+            .bindTo(purchaseButton.rx_enabled)
+            .addDisposableTo(disposeBag)        
+    }
+    
+    //MARK: - Validation methods
+
+    func validateCardTextChange(cardText: String) -> Bool {
+        let noWhitespace = cardText.rw_removeSpaces()
+        
+        updateCardType(noWhitespace)
+        formatCardNumber(noWhitespace)
+        advanceFromCardNumberIfNecessary(noWhitespace)
+        
+        guard cardType.value != .Unknown else {
+            //Definitely not valid if the type is unknown.
+            return false
+        }
+        
+        guard noWhitespace.rw_isLuhnValid() else {
+            //Failed luhn validation
+            return false
+        }
+        
+        return noWhitespace.characters.count == self.cardType.value.expectedDigits
+    }
+    
+    func validateExpirationDateTextChange(expiration: String) -> Bool {
+        let strippedSlashExpiration = expiration.rw_removeSlash()
+        
+        formatFromExpirationDate(strippedSlashExpiration)
+        advanceFromExpirationDateIfNecessary(strippedSlashExpiration)
+        
+        return strippedSlashExpiration.rw_isValidExpirationDate()
+    }
+    
+    func validateCVVTextChange(cvv: String) -> Bool {
+        guard cvv.rw_allCharactersAreNumbers() else {
+            //Someone snuck a letter in here.
+            return false
+        }
+        dismissFromCVVIfNecessary(cvv)
+        return cvv.characters.count == self.cardType.value.cvvDigits
+    }
+    
+    
+    //MARK: Single-serve helper functions
+    
+    private func updateCardType(noSpacesNumber: String) {
+        cardType.value = CardType.fromString(noSpacesNumber)
+    }
+    
+    private func formatCardNumber(noSpacesCardNumber: String) {
+        creditCardNumberTextField.text = self.cardType.value.format(noSpacesCardNumber)
+    }
+    
+    func advanceFromCardNumberIfNecessary(noSpacesNumber: String) {
+        if noSpacesNumber.characters.count == self.cardType.value.expectedDigits {
+            self.expirationDateTextField.becomeFirstResponder()
+        }
+    }
+    
+    func formatFromExpirationDate(expirationNoSpacesOrSlash: String) {
+        expirationDateTextField.text = expirationNoSpacesOrSlash.rw_addSlash()
+    }
+    
+    func advanceFromExpirationDateIfNecessary(expirationNoSpacesOrSlash: String) {
+        if expirationNoSpacesOrSlash.characters.count == 6 { //mmyyyy
+            self.cvvTextField.becomeFirstResponder()
+        }
+    }
+    
+    func dismissFromCVVIfNecessary(cvv: String) {
+        if cvv.characters.count == self.cardType.value.cvvDigits {
+            self.cvvTextField.resignFirstResponder()
+        }
     }
 }
---
+++ Chocotastic/ChocolateIsComingViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ChocolateIsComingViewController.swift
+//  Chocotastic
+//
+//  Created by Ellen Shapiro on 6/7/16.
+//  Copyright © 2016 RayWenderlich.com. All rights reserved.
+//
+
+import UIKit
+
+class ChocolateIsComingViewController: UIViewController {
+
+    @IBOutlet var orderLabel: UILabel!
+    @IBOutlet var costLabel: UILabel!
+    @IBOutlet var creditCardIcon: UIImageView!
+    
+    var cardType: CardType = .Unknown {
+        didSet {
+            configureIconForCardType()
+        }
+    }
+    
+    //MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureIconForCardType()
+        configureLabelsFromCart()
+    }
+    
+    
+    //MARK: - Configuration methods
+    
+    private func configureIconForCardType() {
+        guard let imageView = creditCardIcon else {
+            //View hasn't loaded yet, come back later.
+            return
+        }
+        
+        imageView.image = cardType.image
+    }
+    
+    private func configureLabelsFromCart() {
+        guard let costLabel = costLabel else {
+            //View hasn't loaded yet, come back later.
+            return
+        }
+        
+        let total = ShoppingCart.sharedCart.totalCost()
+        costLabel.text = CurrencyFormatter.dollarsFormatter.stringFromNumber(total)
+    }
+}
